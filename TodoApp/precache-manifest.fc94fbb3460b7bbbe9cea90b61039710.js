a4d28a188c8729128686d67411190e54edfcbc8d
naveenkumar75507@gmail.com <naveenkumar75507@gmail.com>
2019-07-26 09:50:27 +0530

    Todo App

---
+++ TodoApp/precache-manifest.fc94fbb3460b7bbbe9cea90b61039710.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06933f7239c308ded53b6ac9058e8bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c186af4ce6da054e3c3",
+    "url": "/static/js/2.eef387ec.chunk.js"
+  },
+  {
+    "revision": "b01d784eda43ae1ec1d8",
+    "url": "/static/js/main.d18b9f48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
