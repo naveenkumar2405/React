217db773dfe70b54438052f5a49f7b99738fd8f6
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-07-26 09:55:46 +0530

    Update precache-manifest.fc94fbb3460b7bbbe9cea90b61039710.js

--- TodoApp/precache-manifest.fc94fbb3460b7bbbe9cea90b61039710.js
+++ TodoApp/precache-manifest.fc94fbb3460b7bbbe9cea90b61039710.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "06933f7239c308ded53b6ac9058e8bb8",
-    "url": "/index.html"
+    "url": "https://naveenkumar2405.github.io/React/TodoApp/index.html"
   },
   {
     "revision": "3c186af4ce6da054e3c3",
-    "url": "/static/js/2.eef387ec.chunk.js"
+    "url": "https://naveenkumar2405.github.io/React/TodoApp/static/js/2.eef387ec.chunk.js"
   },
   {
     "revision": "b01d784eda43ae1ec1d8",
-    "url": "/static/js/main.d18b9f48.chunk.js"
+    "url": "https://naveenkumar2405.github.io/React/TodoApp/static/js/main.d18b9f48.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "https://naveenkumar2405.github.io/React/TodoApp/static/js/runtime~main.a8a9905a.js"
   }
 ]);
